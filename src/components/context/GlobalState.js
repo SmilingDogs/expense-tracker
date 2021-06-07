e9aab228dd3845b7cc48f17eac1415b219080675
Alexey Kudryashov <a.kudr74@gmail.com>
2021-06-07 12:54:01 +0300

    clean-up

--- src/components/context/GlobalState.js
+++ src/components/context/GlobalState.js
@@ -5,34 +5,39 @@ import { AppReducer } from "./Appreducer";
 //! 1 способ передать стейт всем - это Редакс,  2 -ой: это Context Provider. Здесь будет 2 способ!
 
 const initialState = {
-  transactions: localStorage.getItem('transactions') ? JSON.parse(localStorage.getItem('transactions')) : [],
+  transactions: localStorage.getItem("transactions")
+    ? JSON.parse(localStorage.getItem("transactions"))
+    : [],
 };
 //* create Context
- export const GlobalContext = createContext(initialState)
+export const GlobalContext = createContext(initialState);
 
 //* создаем аналог Стора(редакса) - компонент Provider, в который необхоимо обернуть все приложение чтобы все компоненты получили доступ к стейту(initialState)
 
-export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
-    useEffect(() => {
-        localStorage.setItem('transactions', JSON.stringify(state.transactions))
-    }, [state])
-//* now we need some Actions
-    const deleteTransaction = id => {
-        dispatch ({type:"DELETE_TRANSACTION", payload: id})
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(state.transactions));
+  }, [state]);
+  //* now we need some Actions
+  
+  const deleteTransaction = (id) => {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  };
+  const addTransaction = (transaction) => {
+    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
 
-    }
-    const addTransaction = transaction => {
-        dispatch ({ type:"ADD_TRANSACTION", payload: transaction})
-        localStorage.setItem('transactions', JSON.stringify(state.transaction))
-    }
+  };
 
-    return (<GlobalContext.Provider value={{
+  return (
+    <GlobalContext.Provider
+      value={{
         transactions: state.transactions, //! передаем Стейт в Провайдер, чтобы использовать в разных компонентах
         deleteTransaction, //! ТОчно также - передаем функции в Провайдер
-        addTransaction
-    }}>
-        {children}
+        addTransaction,
+      }}
+    >
+      {children}
     </GlobalContext.Provider>
-    )
-}
+  );
+};
